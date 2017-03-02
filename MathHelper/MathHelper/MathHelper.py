7517f103907a6c404bdc22eebd5780fb8eeb3ff0
alex t <antauren@yandex.ru>
2017-03-02 12:59:05 +0300

    добавлены функции, переменные получили говорящие названия

--- MathHelper/MathHelper/MathHelper.py
+++ MathHelper/MathHelper/MathHelper.py
@@ -1,25 +1,33 @@
-# -*- coding: utf-8 -*-
-
 import sys
 
-print('Здравствуйте, Вас приветсвует программа вычисления среднего арифметического и геометрического списка чисел')
+
+parameter = sys.argv[1]
+list = sys.argv[2:]
 
 
-if sys.argv[1] == '-am':
-    s = 0
-    for i in sys.argv[2:]:
-        s += int(i)
-    print(s/len(sys.argv[2:]))
+def arithmetic_mean(list):
+    sum = 0
+    for i in list:
+        sum += int(i)    
+    return sum / len(list)
 
 
-elif sys.argv[1] == '-gm':
-    G = 1
-    for i in sys.argv[2:]:
-        G *= int(i)
-    print( G ** ( 1/len(sys.argv[2:]) ) )
+def geometric_mean(list):
+    multiplication = 1
+    for i in list:
+        multiplication *= int(i)
+    return multiplication ** (1/len(list))
+    
+if parameter == '-am':
+    print(  arithmetic_mean( list )  ) 
+
+elif parameter == '-gm':
+    print(  geometric_mean( list )  )
+    
     
 
 else:
+    print('Здравствуйте, Вас приветсвует программа вычисления среднего арифметического и геометрического списка чисел')
     print('''Вызовите программу с одним из следующих аргументов:\n
            -am <список чисел> - вычисляет среднее арифметическое для списка чисел.\n
             Разрешаются только положительные числа. Например, -am 1 2 3 4 5\n
@@ -27,4 +35,4 @@ else:
             Разрешаются только положительные числа. Например, -gm 3 3 3''')
 
 
-#print(sys.argv)
+print(sys.argv[1:])
